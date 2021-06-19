8bad818c011ac8456d827cbcd5d39b89dfc788e2
Sergio <sdiaz@us.es>
2021-06-19 16:27:38 +0200

    Initialize spline coefficients with _solve_interpolation()

--- bgoptimizer.py
+++ bgoptimizer.py
@@ -1,3 +1,6 @@
+# %%
+%load_ext autoreload
+%autoreload 2
 
 # %%
 import os
@@ -8,35 +11,26 @@ from tensorflow import keras
 from spline import Spline
 import matplotlib.pyplot as plt
 
-filename = "Ha_nonlinear_r.xisf"
+filename = "Ha_nonlinear_median.xisf"
 
-N = 16
+N = 24
 O = 3
-B = 4
+B = 1
 
 alpha = 1
 
 lr = 0.001
-epochs = 10000
+epochs = 1000
 
 # %%
-xisf = XISF()
-xisf.read(filename)
+xisf = XISF(filename)
 im_orig = xisf.read_image(0)
 im_shape = im_orig.shape
-xisf.close()
 
-# %%
-#plt.imshow(im_orig[:,:,0])
-im_orig = np.expand_dims(im_orig, axis=0)
-
-# %%
-y_true = im_orig.repeat(B, axis=0)
-y_true.shape
 
 # %%
 x = keras.layers.Input(shape=(), name='input_layer', batch_size=B)
-y = Spline(shape=im_shape, control_points=N, order=O)(x)
+y = Spline(im_orig, control_points=N, order=O)(x)
 model = keras.Model(inputs=x, outputs=y, name="bgmodel")
 
 model.summary()
@@ -60,7 +54,7 @@ model.compile(optimizer, loss=bg_loss)
 earlystop = tf.keras.callbacks.EarlyStopping(
     monitor='loss', 
     min_delta=0.0001, 
-    patience=25,
+    patience=50,
     restore_best_weights=True,
     verbose=True
 )
@@ -68,8 +62,8 @@ earlystop = tf.keras.callbacks.EarlyStopping(
 reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
     monitor='loss', 
     factor=0.5,
-    patience=5, 
-    min_lr=0.0001,
+    patience=10, 
+    min_lr=0.00001,
     verbose=True
 )
 
@@ -81,11 +75,25 @@ def lr_sched(epoch, lr):
 
 lrsched = tf.keras.callbacks.LearningRateScheduler(lr_sched, verbose=True)
 
-callbacks = [earlystop] #, reduce_lr, lrsched]
+callbacks = [earlystop, reduce_lr] #, lrsched]
 
 # %%
+# Draw spline train points
+from matplotlib.patches import Rectangle
+
+def plot_train_points():
+    train_points = model.layers[1].train_points.numpy()[0]
+    ax = plt.plot(train_points[:,0], train_points[:,1], 'o')
+    rect = Rectangle((0, 0), 1, 1, linewidth=1, edgecolor='r', facecolor='none')
+    _ = plt.gca().add_patch(rect)
+
+plot_train_points()
+
+# %%
+im_orig = np.expand_dims(im_orig, axis=0)
+y_true = im_orig.repeat(B, axis=0)
 X = np.zeros(B) 
-X.shape
+im_orig.shape, y_true.shape, X.shape
 
 # %%
 history = model.fit(
@@ -94,13 +102,15 @@ history = model.fit(
     callbacks=callbacks
 )
 
+plt.plot(history.history['loss'], label='Loss')
+
 # %%
 y_pred = model.predict_on_batch(X).numpy()
 
-plt.imshow(y_pred[0,...,0], cmap='gray')
+plt.imshow(y_pred[0,...,0], cmap='gray', vmin=0, vmax=1)
 
 # %%
-plt.plot(history.history['loss'], label='Loss')
+plot_train_points()
 
 # %%
 plt.figure(figsize=(16,10))
--- spline.py
+++ spline.py
@@ -1,36 +1,46 @@
 import tensorflow as tf
-from tensorflow_addons.image.interpolate_spline import _apply_interpolation
+from tensorflow_addons.image.interpolate_spline import _apply_interpolation, _solve_interpolation
 from tensorflow import keras
 import numpy as np
 
-# TODO: documentar
+# TODO: batch_size, any effect on convergence?
+
 
 class Spline(keras.layers.Layer):
-    def __init__(self, shape, control_points=16, order=3, **kwargs):
+    def __init__(self, img, control_points=16, order=3, **kwargs):
         super(Spline, self).__init__(**kwargs)
-        self.shape = shape
+        self.img = img
+        self.shape = img.shape
         self.control_points = control_points
         self.order = order
 
     def build(self, input_shape):
         self.batch_size = input_shape[0]
-        h, w, _ = self.shape
+        h, w, _ = self.shape 
 
-        # query_points: todos los puntos (x, y) = ([0:W], [0:H])
+        # query_points: generate every pixel position (x, y) = ([0:W], [0:H])
+        # TODO: generate them on the ([0,1], [0,1]) range
         x, y = np.arange(0, w), np.arange(0, h)
         self.query_points = tf.constant(np.array(np.meshgrid(x, y)).T.reshape(1, -1, 2).astype(np.float32)) 
 
+        # train_points: randomly distributed over ([0,1], [0,1]) range
         train_points_initializer = tf.keras.initializers.RandomUniform(minval=0.0, maxval=1.0)
-        wvalues_initializer = tf.keras.initializers.RandomUniform(minval=-1.0, maxval=1.0)
         self.train_points = self.add_weight(
             shape=(1, self.control_points, 2), initializer=train_points_initializer, trainable=True
         )
-        self.ww = self.add_weight(
-            shape=(1, self.control_points, 1), initializer=wvalues_initializer, trainable=True
-        )
-        self.vw = self.add_weight(
-            shape=(1, 3, 1), initializer=wvalues_initializer, trainable=True
-        )
+
+        # ww, vw spline coefficients: initialize from actual image pixel values on the train_points (scaled
+        # to the image size) for faster convergence (much faster than random initialization)
+        train_coords = self.train_points.numpy().squeeze(axis=0)
+        train_coords = (train_coords * np.array(self.shape[0:2])).astype('int') 
+        train_values = np.expand_dims(self.img[ train_coords[:,0], train_coords[:,0], 0 ], axis=(0,-1))
+        # We use _solve_interpolation for the initial spine coefficient values, 
+        # see https://www.tensorflow.org/addons/api_docs/python/tfa/image/interpolate_spline
+        ww, vw = _solve_interpolation(
+            self.train_points, train_values, self.order, regularization_weight=0.1
+        ) 
+        self.ww = tf.Variable(initial_value=ww, trainable=True)
+        self.vw = tf.Variable(initial_value=vw, trainable=True)
 
         super(Spline, self).build(input_shape)
       
@@ -40,7 +50,7 @@ class Spline(keras.layers.Layer):
 
         # tfa.image._apply_interpolation: interpolación spline poliarmónica.
         # Es una función auxiliar de tfa.image.interpolate_spline(); no podemos usar ésta
-        # directamente ya que determina w y v realizando tf.linalg.solve() y ésta 
+        # directamente durante fit() ya que determina w y v realizando tf.linalg.solve() y ésta 
         # da problemas al encontrarse con matrices no invertibles. 
         #
         # query_points: `[b, m, d]` x values to evaluate the interpolation at.
@@ -59,11 +69,15 @@ class Spline(keras.layers.Layer):
 
         query_points = tf.repeat(self.query_points, self.batch_size, axis=0)
 
-        # Escalamos los puntos de control (x,y)
+        # Clip the train_points to the [0,1] box to constrain them
+        # TODO: make this an option?
+        train_points = keras.backend.clip(self.train_points, 0.0, 1.0)
+        # Escalamos los puntos de control (x,y)        
         train_points = self.train_points * tf.constant([w, h], dtype=tf.float32) 
         train_points = tf.repeat(train_points, self.batch_size, axis=0)
 
         # Los pesos también necesitan escalado... pero cuál ??????????
+        # TODO: don't scale query_values in init() and avoid w*w*h*h scaling here
         w_weights = tf.repeat(self.ww, self.batch_size, axis=0) / (w*w*h*h)
         v_weights = tf.repeat(self.vw, self.batch_size, axis=0)
 
@@ -77,12 +91,22 @@ class Spline(keras.layers.Layer):
 
         # Los valores tienen shape (B, W*H, 1) ordenados por columnas, filas;
         # hay que reestructurar a formato de imagen
+        # TODO: generate query_points as y,x values instead to avoid transposing
         query_values = tf.reshape(query_values, (-1, w, h, 1))
         return tf.transpose(query_values, perm=[0, 2, 1, 3])
 
-# TODO
+    # TODO: quitar, no le veo mucho sentido a almacenar esta capa, lo que interesa
+    # del modelo es entrenarlo, no usarlo con predict. 
     def get_config(self):
         config = super(Spline, self).get_config()
         config.update({"shape": self.shape})
         config.update({"order": self.order})
         return config
+
+    # TODO: sí puede ser interesante añadir un método para generar el spline sobre otro
+    # conjunto de query_points (para la imagen en tamaño original, entiendiendo que el
+    # entrenamiento se hará sobre una imagen reducida)
+    def generate_spline(shape):
+        # generar query_points según shape
+        # use ww y vw y _apply_interpolation para generar spline
+        pass
