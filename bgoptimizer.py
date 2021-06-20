train_points and query_points using [0,1) coordinates to avoid scaling of w spline coefficients. Also swapped x and y coordinates to avoid axis permutation on call(). Corrected a bug on train_values initialization.

bgoptimizer.py
@@ -13,9 +13,9 @@ import matplotlib.pyplot as plt
 
 filename = "Ha_nonlinear_median.xisf"
 
-N = 24
+N = 16
 O = 3
-B = 1
+B = 1 # TODO: batch_size, any effect on convergence?
 
 alpha = 1
 
@@ -78,22 +78,24 @@ lrsched = tf.keras.callbacks.LearningRateScheduler(lr_sched, verbose=True)
 callbacks = [earlystop, reduce_lr] #, lrsched]
 
 # %%
-# Draw spline train points
-from matplotlib.patches import Rectangle
-
-def plot_train_points():
-    train_points = model.layers[1].train_points.numpy()[0]
-    ax = plt.plot(train_points[:,0], train_points[:,1], 'o')
-    rect = Rectangle((0, 0), 1, 1, linewidth=1, edgecolor='r', facecolor='none')
-    _ = plt.gca().add_patch(rect)
-
-plot_train_points()
-
-# %%
 im_orig = np.expand_dims(im_orig, axis=0)
 y_true = im_orig.repeat(B, axis=0)
 X = np.zeros(B) 
 im_orig.shape, y_true.shape, X.shape
+
+# %%
+# Draw spline train points
+import seaborn as sns
+from matplotlib.patches import Rectangle
+
+def plot_train_points():
+    train_points = model.layers[1].train_points.numpy()[0]
+    # values = _apply_interpolation...
+    ax = sns.scatterplot(x=train_points[:,0], y=train_points[:,1]) 
+    rect = Rectangle((0, 0), 1, 1, linewidth=1, edgecolor='r', facecolor='none')
+    _ = plt.gca().add_patch(rect)
+
+plot_train_points()
 
 # %%
 history = model.fit(

spline.py
@@ -3,14 +3,58 @@ from tensorflow_addons.image.interpolate_spline import _apply_interpolation, _so
 from tensorflow import keras
 import numpy as np
 
-# TODO: batch_size, any effect on convergence?
-
-
 class Spline(keras.layers.Layer):
-    def __init__(self, img, control_points=16, order=3, **kwargs):
+    """
+    Generates a spline by poliharmonic interpolation.
+    
+    This implementation is based on tfa.image.interpolate_spline(), which
+    makes the spline interpolation in two steps: first, it solves for a set
+    of coefficients w and v from a set of train_points and train_values; then,
+    it makes the actual interpolation by using those coefficients values at 
+    the train_points and a list of query_points. 
+    
+    In this layer, the trainable weights are the train_point and the spline
+    coefficients w and v. At the initialization of the layer, it uses the 
+    solve phase, tfa.image._solve_interpolation(), on randomly defined 
+    train_points over the image given as attribute im, to determine the 
+    initial values of the weights w and v. 
+    
+    The layer uses these weights to interpolate a spline, evaluating it at 
+    all pixels positions of the image im, i.e, the output of the layer
+    is a image with the same shape as im.
+
+    Attributes:
+        im: image to fit the spline to; expected shape is (h, w, 1).
+        control_points: number of control points for the spline (16 by default)
+        order: 2 (thin-plate spline) o 3 (bicubic spline, default)
+
+    Input shape:
+        Ignored, except for the batch_size.
+
+    Output shape:
+        N-D tensor with shape: (batch_size, h, w, 1).
+    """
+
+    # References:
+    #
+    # See https://www.tensorflow.org/addons/api_docs/python/tfa/image/interpolate_spline
+    #   query_points: [b, m, d] x values to evaluate the interpolation at.
+    #   train_points: [b, n, d] x values that act as the interpolation centers
+    #   w: [b, n, k] weights on each interpolation center.
+    #   v: [b, d, k] weights on each input dimension.        
+    #   query_values: [b, m, k] interpolation results
+    #
+    # Where:
+    #   b = batch_size
+    #   m = number of locations to evaluate (w*h in this case)
+    #   d = input space dimension (2 in this case)
+    #   n = number of train points (parameter of the layer)
+    #   k = output values dimensions (1 in this case)
+
+    def __init__(self, im, control_points=16, order=3, **kwargs):
         super(Spline, self).__init__(**kwargs)
-        self.img = img
-        self.shape = img.shape
+        self.im = im
+        self.shape = im.shape
         self.control_points = control_points
         self.order = order
 
@@ -18,10 +62,9 @@ class Spline(keras.layers.Layer):
         self.batch_size = input_shape[0]
         h, w, _ = self.shape 
 
-        # query_points: generate every pixel position (x, y) = ([0:W], [0:H])
-        # TODO: generate them on the ([0,1], [0,1]) range
-        x, y = np.arange(0, w), np.arange(0, h)
-        self.query_points = tf.constant(np.array(np.meshgrid(x, y)).T.reshape(1, -1, 2).astype(np.float32)) 
+        # query_points: generate every pixel position, on the ([0,1], [0,1]) range
+        y, x = np.linspace(0, 1, h, endpoint=False), np.linspace(0, 1, w, endpoint=False)
+        self.query_points = tf.constant(np.array(np.meshgrid(y, x)).T.reshape(1, -1, 2).astype(np.float32)) 
 
         # train_points: randomly distributed over ([0,1], [0,1]) range
         train_points_initializer = tf.keras.initializers.RandomUniform(minval=0.0, maxval=1.0)
@@ -29,13 +72,12 @@ class Spline(keras.layers.Layer):
             shape=(1, self.control_points, 2), initializer=train_points_initializer, trainable=True
         )
 
-        # ww, vw spline coefficients: initialize from actual image pixel values on the train_points (scaled
+        # w, v spline coefficients: initialize from actual image pixel values on the train_points (scaled
         # to the image size) for faster convergence (much faster than random initialization)
         train_coords = self.train_points.numpy().squeeze(axis=0)
-        train_coords = (train_coords * np.array(self.shape[0:2])).astype('int') 
-        train_values = np.expand_dims(self.img[ train_coords[:,0], train_coords[:,0], 0 ], axis=(0,-1))
-        # We use _solve_interpolation for the initial spine coefficient values, 
-        # see https://www.tensorflow.org/addons/api_docs/python/tfa/image/interpolate_spline
+        train_coords = (train_coords * np.array(self.shape[:-1])).astype('int') # nearest pixel position
+        train_values = np.expand_dims(self.im[ train_coords[:,0], train_coords[:,1], 0 ], axis=(0,-1))
+        # We use tfa.image._solve_interpolation for the initial spine coefficient values
         ww, vw = _solve_interpolation(
             self.train_points, train_values, self.order, regularization_weight=0.1
         ) 
@@ -48,39 +90,17 @@ class Spline(keras.layers.Layer):
     def call(self, inputs):
         h, w, _ = self.shape
 
-        # tfa.image._apply_interpolation: interpolación spline poliarmónica.
-        # Es una función auxiliar de tfa.image.interpolate_spline(); no podemos usar ésta
-        # directamente durante fit() ya que determina w y v realizando tf.linalg.solve() y ésta 
-        # da problemas al encontrarse con matrices no invertibles. 
-        #
-        # query_points: `[b, m, d]` x values to evaluate the interpolation at.
-        # train_points: `[b, n, d]` x values that act as the interpolation centers
-        #     (the c variables in the wikipedia article).
-        # w: `[b, n, k]` weights on each interpolation center.
-        # v: `[b, d, k]` weights on each input dimension.        
-        # Returns: `[b, m, k]` float `Tensor` of query values
-        #
-        # Donde:
-        # b = batch_size
-        # m = número de puntos a evaluar, w*h
-        # d = 2 (dimensión del espacio de entrada, 2D)
-        # n = número de puntos de control (hiperparámetro)
-        # k = 1 (dimensión de los valores de salida)
+        # Clip the train_points to the [0,1] box to constrain them
+        # TODO: make this optional?
+        train_points = keras.backend.clip(self.train_points, 0.0, 1.0)
 
+        # Broadcast the weights and query_points to the batch_size
+        train_points = tf.repeat(train_points, self.batch_size, axis=0)
+        w_weights = tf.repeat(self.ww, self.batch_size, axis=0)# / (w*w*h*h)
+        v_weights = tf.repeat(self.vw, self.batch_size, axis=0)
         query_points = tf.repeat(self.query_points, self.batch_size, axis=0)
 
-        # Clip the train_points to the [0,1] box to constrain them
-        # TODO: make this an option?
-        train_points = keras.backend.clip(self.train_points, 0.0, 1.0)
-        # Escalamos los puntos de control (x,y)        
-        train_points = self.train_points * tf.constant([w, h], dtype=tf.float32) 
-        train_points = tf.repeat(train_points, self.batch_size, axis=0)
-
-        # Los pesos también necesitan escalado... pero cuál ??????????
-        # TODO: don't scale query_values in init() and avoid w*w*h*h scaling here
-        w_weights = tf.repeat(self.ww, self.batch_size, axis=0) / (w*w*h*h)
-        v_weights = tf.repeat(self.vw, self.batch_size, axis=0)
-
+        # Perform the interpolation, output shape is (batch_size, h*w, 1)
         query_values = _apply_interpolation(
             query_points=self.query_points,
             train_points=train_points,
@@ -89,19 +109,8 @@ class Spline(keras.layers.Layer):
             order=self.order,
         )
 
-        # Los valores tienen shape (B, W*H, 1) ordenados por columnas, filas;
-        # hay que reestructurar a formato de imagen
-        # TODO: generate query_points as y,x values instead to avoid transposing
-        query_values = tf.reshape(query_values, (-1, w, h, 1))
-        return tf.transpose(query_values, perm=[0, 2, 1, 3])
-
-    # TODO: quitar, no le veo mucho sentido a almacenar esta capa, lo que interesa
-    # del modelo es entrenarlo, no usarlo con predict. 
-    def get_config(self):
-        config = super(Spline, self).get_config()
-        config.update({"shape": self.shape})
-        config.update({"order": self.order})
-        return config
+        # Reshape to (batch_size, h, w, 1)
+        return tf.reshape(query_values, (-1, h, w, 1))
 
     # TODO: sí puede ser interesante añadir un método para generar el spline sobre otro
     # conjunto de query_points (para la imagen en tamaño original, entiendiendo que el