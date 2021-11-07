74f0d7d7403a310867787c00b98dc0d33a3d77cf
Sergio <sdiaz@us.es>
2021-11-07 12:12:46 +0100

    Spline modified to accept grayscale and RGB im input

--- spline.py
+++ spline.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow_addons.image.interpolate_spline import _apply_interpolation, _solve_interpolation
 from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Spline(keras.layers.Layer):
@@ -65,7 +66,7 @@ class Spline(keras.layers.Layer):
     #   m = number of locations to evaluate (w*h in this case)
     #   d = input space dimension (2 in this case)
     #   n = number of train points (parameter of the layer)
-    #   k = output values dimensions (1 in this case)
+    #   k = output values dimensions (1 or 3, for mono or RGB images, resp.)
 
     def __init__(self, im, mask=1.0, n_control_points=64, order=2, initializer='random', initial_spline_regularization=0.1, **kwargs):
         super(Spline, self).__init__(**kwargs)
@@ -88,7 +89,7 @@ class Spline(keras.layers.Layer):
         # From now on, self.mask is either a tuple (float, float) or a ndarray.
 
         self.shape = im.shape
-        self.bg_val = np.median(self.im)        
+        self.bg_val = np.median(self.im, axis=(0, 1))
         self.n_control_points = n_control_points
         self.order = order
         self.initializer = initializer
@@ -96,7 +97,7 @@ class Spline(keras.layers.Layer):
 
 
     @staticmethod
-    def _apply_mask(im, thresholds):
+    def _generate_mask(im, thresholds):
         thr_min, thr_max = thresholds
         return ((thr_min <= im) & (im <= thr_max)).astype(np.float32)
 
@@ -112,7 +113,7 @@ class Spline(keras.layers.Layer):
 
         # if mask is given as a threshold range, convert it to ndarray
         if isinstance(self.mask, tuple): 
-            self.mask = self._apply_mask(self.im, self.mask)
+            self.mask = self._generate_mask(self.im, self.mask)
         
         # Checks
         assert isinstance(self.mask, np.ndarray) # At this point, mask should be a ndarray
@@ -140,22 +141,21 @@ class Spline(keras.layers.Layer):
         train_coords = (train_coords * np.array([h, w])).astype(np.int32) # nearest pixel position
 
         # Evaluate pixel values from the image and the mask, using train_coords pixel positions
-        pixel_values = self.im[ train_coords[:,0], train_coords[:,1], 0 ]
-        mask_values = self.mask[ train_coords[:,0], train_coords[:,1], 0 ]
+        pixel_values = self.im[ train_coords[:,0], train_coords[:,1], : ]
+        mask_values = self.mask[ train_coords[:,0], train_coords[:,1], : ]
         # Initial train_values are evaluated as follows:
         # - Fully unmasked pixels: actual pixel value
         # - Fully masked pixels: estimated background value (median)
         # - Partially masked pixels: linear combination of the above
         train_values = mask_values * pixel_values + (1-mask_values) * self.bg_val
 
-        # Expand train_values for batch and output axis
-        train_values = np.expand_dims(train_values, axis=(0,-1))
+        # Expand train_values for batch axis
+        train_values = np.expand_dims(train_values, axis=0)
 
         # Convert im, mask to tensors
         self.im, self.mask = tf.constant(self.im), tf.constant(self.mask)       
 
         # We use tfa.image._solve_interpolation for the initial spine coefficient values
-        # TODO: make regularization_weight an argument?
         ww, vw = _solve_interpolation(
             self.train_points, train_values, self.order, regularization_weight=self.initial_spline_regularization
         ) 
@@ -167,7 +167,7 @@ class Spline(keras.layers.Layer):
       
 
     def call(self, inputs):
-        h, w, _ = self.shape
+        h, w, k = self.shape
 
         # Broadcast the weights and query_points to the batch_size
         #   TODO: clip the train_points to the [0,1] box to constrain them?
@@ -177,7 +177,7 @@ class Spline(keras.layers.Layer):
         v_weights = tf.repeat(self.vw, self.batch_size, axis=0)
         query_points = tf.repeat(self.query_points, self.batch_size, axis=0)
 
-        # Perform the interpolation, output shape is (batch_size, h*w, 1)
+        # Perform the interpolation, output shape is (batch_size, h*w, k)
         query_values = _apply_interpolation(
             query_points=query_points,
             train_points=train_points,
@@ -186,17 +186,17 @@ class Spline(keras.layers.Layer):
             order=self.order,
         )
 
-        # Reshape to (batch_size, h, w, 1)
-        return tf.reshape(query_values, (-1, h, w, 1))
+        # Reshape to (batch_size, h, w, k)
+        return tf.reshape(query_values, (-1, h, w, k))
 
 
     # Training is usually done on a downscaled image; this method allows to 
     # generate the trained spline on any shape = (h,w,...) (usually (h,w,1)). 
     #
     # Note: this has to be done in chunks, because _apply_interpolation()
-    # gives an OOM error trying to MatMul if query_points is large.  
-    # chunks = downscale_factor**2 avoids OOM, if the spline could train with that
-    # downscale_factor.
+    # may raise an OOM error trying to MatMul if query_points is large.  
+    # chunks = downscale_factor**2 should avoid OOM, if the spline could train 
+    # over a image downscaled with that factor.
     def interpolate(self, shape, chunks):
         h, w = shape[:2]
 
@@ -212,7 +212,7 @@ class Spline(keras.layers.Layer):
             end = min(start + chunk_sz, query_points.shape[1])
             print(f"{100*i//chunks}..", end='')
 
-            query_points_chunk = query_points[:,start:end,:]
+            query_points_chunk = query_points[:, start:end, :]
             query_values_chunk = _apply_interpolation(
                 query_points=query_points_chunk,
                 train_points=self.train_points,
@@ -226,3 +226,15 @@ class Spline(keras.layers.Layer):
         print("100%")
 
         return bg.numpy()
+
+
+    def plot_train_points(self):
+        h, w, _ = self.shape 
+
+        train_points = self.train_points.numpy()[0, ...]
+        im_masked = (self.im * self.mask).numpy()
+
+        fig = plt.figure(figsize=(16,8))
+        plt.imshow(np.squeeze(im_masked), vmin=0, vmax=1)
+        plt.plot(train_points[:,1]*w, train_points[:,0]*h, 'ro', fillstyle='none')
+        plt.show()
